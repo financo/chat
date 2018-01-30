febce555d46ae3c36ed51ddafae1db4357434a06
wangzhengyu <121260020@qq.com>
2018-01-30 20:41:48 +0800

    0.9

--- src/com/wzy/ChatServer.java
+++ src/com/wzy/ChatServer.java
@@ -1,7 +1,9 @@
 package com.wzy;
 
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,24 +11,48 @@ public class ChatServer {
 	
 	public static void main(String[] args) {
 		boolean started = false;
+		ServerSocket ss = null;
+		Socket s = null;
+		DataInputStream dis = null;
 		try {
-			ServerSocket ss = new ServerSocket(8888);
+			ss = new ServerSocket(8888);
+		}catch (BindException e) {
+			System.out.println("端口使用中...");
+			System.out.println("请关闭相关程序，并重新运行！");
+			System.exit(0);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
 			started = true;
 			while(started) {
 				boolean bConnected = false;
-				Socket s = ss.accept();
+				s = ss.accept();
 System.out.println("a client connected!");
 				bConnected = true;
-				DataInputStream dis = new DataInputStream(s.getInputStream());
+				dis = new DataInputStream(s.getInputStream());
 				while(bConnected) {
 					String str = dis.readUTF();
 					System.out.println(str);
 				}
-				dis.close();
+				//dis.close();
 			}
-		} catch (IOException e) {
+		}catch (EOFException e) {
+			System.out.println("Client closed!");
+		}catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				if (dis != null) {
+					dis.close();
+				}
+				if (s != null) {
+					s.close();
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
-		
 	}
 }
